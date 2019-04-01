68b5c2940243505d94af4c55879e12b39129f990
Moudou <will@kingfor.com>
2019-04-01 13:49:19 +0800

    Add a sample project.

---
+++ SourceCode/FileReader/Moudou.FileReader.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moudou.FileReader.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileLineReader reader = new FileLineReader();
+
+            var enm = reader.ReadEachLine("D:\\Logs\\ue190315.log");
+
+            foreach (string[] columns in enm)
+            {
+
+            }
+        }
+    }
+}
